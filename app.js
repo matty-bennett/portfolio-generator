99b808e058c43956407e237386f954e5fd4a21f4
matty-bennett <bennyot10@gmail.com>
2021-03-18 18:02:51 -0600

    modularized generate page which dynamically creates html

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+/*const printProfileData = profileDataArr => {
+  // This...
+  for (let i = 0; i < profileDataArr.length; i += 1) {
+    console.log(profileDataArr[i]);
+  }
 
-const printProfileData = profileDataArr => {
-    profileDataArr.forEach(profileItem => 
-        console.log(profileItem)
-    );
+  console.log('================');
+
+  // Is the same as this...
+  profileDataArr.forEach(profileItem => console.log(profileItem));
 };
 
-printProfileData(profileDataArgs);
+printProfileData(profileDataArgs); */
+
+const fs = require('fs');
+
+const generatePage = require('./src/page-template');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!')
+});
