bc2fb28482783d6e66da44f3ed72aec6195ef4a9
matty-bennett <bennyot10@gmail.com>
2021-03-21 22:00:57 -0600

    created promise based functions for copy and writefile functions

--- app.js
+++ app.js
@@ -15,8 +15,9 @@ printProfileData(profileDataArgs); */
 // if node modules is added to git use: git rm -r --cached node_modules 
 
 const inquirer = require('inquirer');
-const fs = require('fs');
+const {writeFile, copyFile} = require('./utils/generate-site.js');
 const generatePage = require('./src/page-template');
+const { writeFile, copyFile } = require('node:fs');
 
 // const pageHTML = generatePage(name, github);
 
@@ -203,11 +204,38 @@ const pageHTML = generatePage(mockData);
  promptUser()
    .then(promptProject)
    .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
+      return generatePage(portfolioData);
+   })
+   .then(pageHTML => {
+     return writeFile(pageHTML);
+   })
+   .then(writeFileResponse => {
+     console.log(writeFileResponse);
+     return copyFile();
+   })
+   .then(copyFileResponse => {
+     console.log(copyFileResponse);
+   })
+   .catch(err => {
+     console.log(err);
+   });
 
-     fs.writeFile('./index.html', pageHTML, err => {
-      if (err) throw new Error(err);
+    /* const pageHTML = generatePage(portfolioData);
 
-    console.log('Page created! Check out index.html in this directory to see it!');
+     fs.writeFile('./dist/index.html', pageHTML, err => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log('Page created! Check out index.html in this directory to see it!');
+
+      fs.copyFile('./src/style.css', './dist/style.css', err => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log('Style sheet copied succesfully!');
+      });
     });
-  });
+  }); 
+  */
